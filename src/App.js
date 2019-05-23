db900184baf6ebe0d8710063cd71e8d7d379fd1b
Daniel Goldman <dzgoldman@wesleyan.edu>
2019-05-23 16:04:38 -0400

    rendering film details

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import TMDB from './TMDB.js';
 import FilmListing from './FilmListing.js';
 import FilmDetails from './FilmDetails.js';
-
+import axios from 'axios';
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -23,7 +23,17 @@ class App extends Component {
 
   setCurrentFilm(film) {
     // take in a film as an argument
-    console.log('setting current film', film)
+    const url = `https://api.themoviedb.org/3/movie/${film.id}?api_key=${TMDB.api_key}&append_to_response=videos,images&language=en`
+    axios.get(url)  
+    .then((res)=>{
+      this.setState({
+        current: res.data
+      })
+    })
+    .catch((err)=>{
+      console.warn(err)
+    })
+
     // make an API request to get more data about that film
     // set the result to current
   }
--- src/FilmDetails.js
+++ src/FilmDetails.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
 
 class FilmDetails extends Component {
+  
   render() {
+    const {current} = this.props
     return (
       <div className="film-details">
         <h1 className="section-title">DETAILS</h1>
+        <br/><br/><br/>
+
+        
+        <div>{current.overview}</div>
+        
       </div>
     )
   }
